405caa0dac666738ddeb34910e4ca2ce4a09dabb
torinunna <torinunna9588@gmail.com>
2022-03-13 21:42:17 +0900

    111. update the storyLabel and Button Titles

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,10 +14,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    let story0 = "You see a fork in the road."
+    let choice1 = "Take a left."
+    let choice2 = "Take a right."
+    
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        storyLabel.text = story0
+        
+        choice1Button.setTitle(choice1, for: .normal)
+        choice2Button.setTitle(choice2, for: .normal)
 
     }
+  
 
 
 }
